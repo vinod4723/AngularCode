f1c9243a2f12d0c3455284778eae352569bad31c
vinod.singh01 <vinod.singh01@IRIS-CSG-901>
2017-03-30 12:35:38 +0530

    change comment

--- TestAng/Controllers/HomeController.cs
+++ TestAng/Controllers/HomeController.cs
@@ -6,10 +6,12 @@ using System.Web.Mvc;
 
 namespace TestAng.Controllers
 {
-
+    /// <summary>
+    /// new class added for index controller
+    /// </summary>
     public class HomeController : Controller
     {
-          public ActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
