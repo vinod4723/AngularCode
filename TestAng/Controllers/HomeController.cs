68d3989571926c841679cd4d5af865c5077cd1ab
Vinod Kumar Singh01 <vinod.singh01@irissoftware.com>
2017-03-30 12:29:40 +0530

    test

--- TestAng/Controllers/HomeController.cs
+++ TestAng/Controllers/HomeController.cs
@@ -9,7 +9,6 @@ namespace TestAng.Controllers
 
     public class HomeController : Controller
     {
-        // GET: Home
           public ActionResult Index()
         {
             return View();
